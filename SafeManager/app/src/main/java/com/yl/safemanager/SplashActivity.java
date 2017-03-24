8196a0f699799a9197bace09b5d0d9a17f8f04b8
YangLang <1004145468@qq.com>
2017-03-24 11:48:51 +0800

    1. 统一聊天界面和应用界面的默认图  2. 修改主界面默认图  3.修改splash展示图

--- SafeManager/app/src/main/java/com/yl/safemanager/SplashActivity.java
+++ SafeManager/app/src/main/java/com/yl/safemanager/SplashActivity.java
@@ -19,7 +19,6 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        //TODO 获取TOken
     }
 
     @Override
